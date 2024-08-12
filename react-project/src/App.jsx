8baff6a04260c627fb6d0518d95fa95e47b609d6
nicolasnovello <nicolas.novello.16@gmail.com>
2024-08-12 07:03:01 -0300

    Añadimos contador

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import { ItemCount } from "./components/ItemCount/ItemCount";
+
 function App() {
   return (
     <>
       <Navbar />
       <ItemListContainer greeting="PROXIMAMENTE..." />
+      <ItemCount />
       <Footer />
     </>
   );
